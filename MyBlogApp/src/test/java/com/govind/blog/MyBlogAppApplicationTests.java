7d1d0f32ea16d5a9e58f4ab037b3383d82838587
Govind Sharma <govindsharma1918@gmail.com>
2023-10-18 21:52:11 +0530

    Committing to local repository

---
+++ MyBlogApp/src/test/java/com/govind/blog/MyBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govind.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
